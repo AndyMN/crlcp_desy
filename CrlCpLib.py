aed11668d8711521f607bf6a0b2219ff1fe879be
Andy Martinez <am41119@hotmail.com>
2016-07-24 10:52:16 +0200

    Add set status vars

--- CrlCpLib.py
+++ CrlCpLib.py
@@ -11,19 +11,17 @@ class CrlCpLib:
 
     def check_credentials(self):
         r = requests.get(self.base_url, auth=(self.user, self.password))
-        self.status_code = r.status_code
+        self._set_status_vars(r.status_code, r.reason)
         self.status_code_should_be(200)
 
     def copy_file_to(self, filename, destination):
         with open(filename, 'rb') as data:
             r = requests.put(self.base_url + destination, data=data, auth=(self.user, self.password))
-        self.status_code = r.status_code
-        self.status_text = r.reason
+        self._set_status_vars(r.status_code, r.reason)
 
     def delete_file(self, filename):
         r = requests.delete(self.base_url + filename, auth=(self.user, self.password))
-        self.status_code = r.status_code
-        self.status_text = r.reason
+        self._set_status_vars(r.status_code, r.reason)
 
     def download_file_as(self, file_to_download, local_filename):
         r = requests.get(self.base_url + file_to_download, stream=True, auth=(self.user, self.password))
@@ -31,8 +29,7 @@ class CrlCpLib:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
-        self.status_code = r.status_code
-        self.status_text = r.reason
+        self._set_status_vars(r.status_code, r.reason)
 
     def status_code_should_be(self, expected_status_code):
         if not int(expected_status_code) == self.status_code:
@@ -41,3 +38,7 @@ class CrlCpLib:
     def status_text_should_be(self, expected_status_text):
         if not expected_status_text == self.status_text:
             raise AssertionError("Expected status text: " + expected_status_text + " but got: " + self.status_text)
+
+    def _set_status_vars(self, status_code, status_text):
+        self.status_code = status_code
+        self.status_text = status_text
